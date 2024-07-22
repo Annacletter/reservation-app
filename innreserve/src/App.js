4cc80eadf6972953490657e90dd597ab150f5e90
Annacletter <anna.cletter12@gmail.com>
2024-07-22 13:01:01 -0700

    Updated Routes

--- innreserve/src/App.js
+++ innreserve/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Reservations from './pages/Reservations';
 import './styles.css';
 
 const App = () => (
   <Router>
-    <Switch>
+    <Routes>
       <Route exact path="/" component={Home} />
       <Route path="/reservations" component={Reservations} />
-    </Switch>
+    </Routes>
   </Router>
 );
 

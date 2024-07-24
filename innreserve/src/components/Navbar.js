c9ee560217b78c479227a65768230bb80b12e37d
Lulekoh <lulekoleoquintus12@gmail.com>
2024-07-24 16:33:52 -0700

    narbar

--- innreserve/src/components/Navbar.js
+++ innreserve/src/components/Navbar.js
@@ -5,22 +5,15 @@ import "../styles.css";
 const navbar= () =>{
     return (
         <div>
-            <Navbar bg="dark" variant="dark" className="app_navbar">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="brand">
-                    <Link to="/">Starterkit</Link>
-                </div>
-                <div className="app_navbar-left">
-                    <div class="search-box">
-                        <button class="btn-search"><FaSearch /></button>
-                        <input type="text" className="input-search" placeholder="Search..." required />
-                    </div>
-                </div>
                 <div className="app_navbar-right">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/Booking">Booking</Link></li>
 	            <li><Link to="/contact">Contact</Link></li>
                     <li><Link to="/about">About</Link></li>
                 </div>
+	    </div>
             </Navbar>
         </div>
     );

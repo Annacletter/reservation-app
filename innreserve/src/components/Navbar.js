bf6d503a9a6fd4a9c72c494a347b63460edb9ac1
Lulekoh <lulekoleoquintus12@gmail.com>
2024-07-24 16:44:19 -0700

    narbar

--- innreserve/src/components/Navbar.js
+++ innreserve/src/components/Navbar.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "../styles.css";
+ 1 import React from 'react';
+ 2 import { Link } from 'react-router-dom';
+ 3 import "../styles.css";
+ 4 
+ 5 const Navbar = () => {
+ 6     return (
+ 7         <div>
+ 8             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+ 9                 <div className="brand">
+10                     <div className="app_navbar-right">
+11                         <li><Link to="/">Home</Link></li>
+12                         <li><Link to="/Booking">Booking</Link></li>
+13                         <li><Link to="/contact">Contact</Link></li>
+14                         <li><Link to="/about">About</Link></li>
+15                     </div>
+16                 </div>
+17             </nav>
+18         </div>
+19     );
+20 }
 
-const navbar= () =>{
-    return (
-        <div>
-            <Navbar className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="brand">
-                <div className="app_navbar-right">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/Booking">Booking</Link></li>
-	            <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                </div>
-	    </div>
-            </Navbar>
-        </div>
-    );
-}
-export default navbar;
+21 export default Navbar;
+

777f60843cd1b0128a6f8d8bbdcebaebe30d4b60
Annacletter <anna.cletter12@gmail.com>
2024-07-25 08:20:17 -0700

    added contact route

--- innreserve/src/components/Navbar.js
+++ innreserve/src/components/Navbar.js
@@ -7,9 +7,10 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <div className="navbar-brand">
-                    InnReserve
-                </div>
+                <a className="navbar-brand" href="/">InnReserve</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
